af03490e0794157ce37b0518181741c3076e0f31
cyr-ius <cyr-ius@ipocus.net>
2026-06-07 12:40:46 +0000

    refactor: remove unused SERVICE constant from const.py

--- packageobspy/const.py
+++ packageobspy/const.py
@@ -2,7 +2,4 @@
 
 TIMEOUT = 30
 TOKEN_ENDPONT = "https://portail-api.meteofrance.fr/token"
-SERVICE = (
-    "/public/DPPaquetObs/v1/paquet/infrahoraire-6m?format=json&id_station=72181001"
-)
 API_URL = "https://public-api.meteofrance.fr/public"
